5d040aafcb8e614b4c301527163e5041921d6729
Ian Adson <58149052+downbios@users.noreply.github.com>
2026-01-06 00:33:47 +0000

    add `database.js` and `.env`files

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-test("GET para api/v1/status deve retornar sucesso 200", async () => {
+test("GET para /api/v1/status deve retornar sucesso 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
   expect(res.status).toBe(200);
 });
